971ad5cebf67725733629c6f22514755adb32945
nori.motoki <1102bad@gmail.com>
2024-04-14 02:03:56 +0900

    CreateServlet.javaを作成

---
+++ src/controllers/CreateServlet.java
@@ -0,0 +1,56 @@
+package controllers;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.tasks_kanri;
+import utils.DBUtil;
+
+
+@WebServlet("/create")
+public class CreateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public CreateServlet() {
+        super();
+
+    }
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+            em.getTransaction().begin();
+
+            tasks_kanri m = new tasks_kanri();
+
+
+            String content = request.getParameter("content");
+            m.setContent(content);
+
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            m.setCreated_at(currentTime);
+            m.setUpdated_at(currentTime);
+
+            em.persist(m);
+            em.getTransaction().commit();
+            em.close();
+
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
+    }
+}
+
