56c4e90e91184426f96599becf5089d8c2534b12
nori.motoki <1102bad@gmail.com>
2024-04-16 00:33:38 +0900

    ShowServlet.javaを作成

--- src/controllers/ShowServlet.java
+++ src/controllers/ShowServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.tasks_kanri;
+import models.Tasks_kanri;
 import utils.DBUtil;
 
 
@@ -28,12 +28,12 @@ public class ShowServlet extends HttpServlet {
         EntityManager em = DBUtil.createEntityManager();
 
         // 該当のIDのメッセージ1件のみをデータベースから取得
-        tasks_kanri m = em.find(tasks_kanri.class, Integer.parseInt(request.getParameter("id")));
+        Tasks_kanri m = em.find(Tasks_kanri.class, Integer.parseInt(request.getParameter("id")));
 
         em.close();
 
         // メッセージデータをリクエストスコープにセットしてshow.jspを呼び出す
-        request.setAttribute("tasks_kanri", m);
+        request.setAttribute("Tasks_kanri", m);
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/show.jsp");
         rd.forward(request, response);
