2926d97da5a10b043938e0527a160bf75b91a073
nori.motoki <1102bad@gmail.com>
2024-04-16 00:49:26 +0900

    存在しないidに対する処理を追加

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -18,30 +18,29 @@ import utils.DBUtil;
 public class EditServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public EditServlet() {
         super();
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        // 該当のIDのメッセージ1件のみをデータベースから取得
+        // 該当のIDのタスク管理1件のみをデータベースから取得
         Tasks_kanri m = em.find(Tasks_kanri.class, Integer.parseInt(request.getParameter("id")));
 
         em.close();
 
-        // メッセージ情報とセッションIDをリクエストスコープに登録
+        // タスク管理情報とセッションIDをリクエストスコープに登録
         request.setAttribute("Tasks_kanri", m);
         request.setAttribute("_token", request.getSession().getId());
 
+        // タスク管理データが存在しているときのみ
         // メッセージIDをセッションスコープに登録
-        request.getSession().setAttribute("Tasks_kanri_id", m.getId());
+        if(m != null) {
+            request.getSession().setAttribute("tasks_kanri_id", m.getId());
+        }
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
