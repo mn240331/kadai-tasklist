afa65f68bcd55e207282d20bb690145f9f5be802
nori.motoki <1102bad@gmail.com>
2024-04-16 23:49:07 +0900

    DestroyServlet.javaを作成

--- src/controllers/DestroyServlet.java
+++ src/controllers/DestroyServlet.java
@@ -20,19 +20,20 @@ public class DestroyServlet extends HttpServlet {
         super();
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         String _token = request.getParameter("_token");
-        if(_token != null && _token.equals(request.getSession().getId())) {
+        if (_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
-            // セッションスコープからメッセージのIDを取得して
+            // セッションスコープからタスクのIDを取得して
             // 該当のIDのタスク管理1件のみをデータベースから取得
-            Tasks_kanri m = em.find(Tasks_kanri.class, (Integer)(request.getSession().getAttribute("Tasks_kanri_id")));
+            Tasks_kanri m = em.find(Tasks_kanri.class, (Integer) (request.getSession().getAttribute("Tasks_kanri_id")));
 
             em.getTransaction().begin();
-            em.remove(m);       // データ削除
+            em.remove(m); // データ削除
             em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "削除が完了しました。");       // ここを追記
+            request.getSession().setAttribute("flush", "削除が完了しました。"); // ここを追記
             em.close();
 
             // セッションスコープ上の不要になったデータを削除
