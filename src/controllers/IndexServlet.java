7fc22e69384852e51a2835b7029f23bafeb816aa
nori.motoki <1102bad@gmail.com>
2024-04-16 23:43:01 +0900

    IndexServlet.javaを作成

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -16,41 +16,43 @@ import utils.DBUtil;
 
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
-        private static final long serialVersionUID = 1L;
-
+    private static final long serialVersionUID = 1L;
 
     public IndexServlet() {
         super();
 
     }
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
         // 開くページ数を取得（デフォルトは1ページ目）
         int page = 1;
         try {
             page = Integer.parseInt(request.getParameter("page"));
-        } catch(NumberFormatException e) {}
+        } catch (NumberFormatException e) {
+        }
 
         // 最大件数と開始位置を指定してメッセージを取得
         List<Tasks_kanri> tasks = em.createNamedQuery("getAlltasks", Tasks_kanri.class)
-                                   .setFirstResult(15 * (page - 1))
-                                   .setMaxResults(15)
-                                   .getResultList();
+                .setFirstResult(15 * (page - 1))
+                .setMaxResults(15)
+                .getResultList();
 
         // 全件数を取得
-        long tasks_count = (long)em.createNamedQuery("gettasksCount", Long.class)
-                                      .getSingleResult();
+        long tasks_count = (long) em.createNamedQuery("gettasksCount", Long.class)
+                .getSingleResult();
 
         em.close();
 
         request.setAttribute("tasks", tasks);
-        request.setAttribute("tasks_count", tasks_count);     // 全件数
-        request.setAttribute("page", page);                         // ページ数
+        request.setAttribute("tasks_count", tasks_count); // 全件数
+        request.setAttribute("page", page); // ページ数
 
         // フラッシュメッセージがセッションスコープにセットされていたら
         // リクエストスコープに保存する（セッションスコープからは削除）
-        if(request.getSession().getAttribute("flush") != null) {
+        if (request.getSession().getAttribute("flush") != null) {
             request.setAttribute("flush", request.getSession().getAttribute("flush"));
             request.getSession().removeAttribute("flush");
         }
